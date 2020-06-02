88f22b8b9a5c05d8a2ab33a019cd80342708d7c9
Aditi Gupta <60029443+Aditi200@users.noreply.github.com>
2020-06-02 15:34:58 +0530

    Add files via upload

---
+++ invertbinarytree.java
@@ -0,0 +1,13 @@
+Invert Binary Tree
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+        return null;
+    }
+    TreeNode right = invertTree(root.right);
+    TreeNode left = invertTree(root.left);
+    root.left = right;
+    root.right = left;
+    return root;
+}
+}
